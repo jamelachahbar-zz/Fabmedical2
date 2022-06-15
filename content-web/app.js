959cda98a8f92d4bb570c7836547a701a0a95bcd
jamelachahbar <jamel.achahbar@gmail.com>
2022-06-15 12:59:25 +0000

    Added Application Insights

--- content-web/app.js
+++ content-web/app.js
@@ -4,6 +4,10 @@ const path = require('path');
 const request = require('request');
 
 const app = express();
+const appInsights = require("applicationinsights");
+appInsights.setup("[YOUR APPINSIGHTS KEY]");
+appInsights.start();
+
 
 app.use(express.static(path.join(__dirname, 'dist/content-web')));
 const contentApiUrl = process.env.CONTENT_API_URL || "http://localhost:3001";
